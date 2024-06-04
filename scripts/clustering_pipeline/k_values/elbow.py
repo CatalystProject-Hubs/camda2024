2a9534c343fd5ab416eaa72378619de5ea2424dc
scLiliana <123580514+scLiliana@users.noreply.github.com>
2024-06-04 14:45:16 -0600

    Metodo elbow

---
+++ scripts/clustering_pipeline/k_values/elbow.py
@@ -0,0 +1,39 @@
+pip install kneed
+
+# Librerias
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+
+from sklearn.cluster import KMeans
+from kneed import KneeLocator
+
+
+def printElbow(K, sse, knee):
+     # Crear un gráfico para visualizar la prueba del codo
+    plt.plot(K, sse, 'bx-')
+    plt.xlabel('Número de clusters (K)')
+    plt.ylabel('Suma de los cuadrados de los errores (SSE)')
+    plt.title('Prueba del codo')
+    
+    plt.axvline(knee.knee, color='r', linestyle='--')
+    plt.text(knee.knee, sse[knee.knee-1], f"K = {knee.knee}", ha='center', va='bottom', fontsize=12)
+    plt.show()
+
+def elbow(data):
+    K = range(1, 20)
+    sse = []
+    
+    for k in K:
+        kmeans = KMeans(n_clusters=k, random_state=42, n_init="auto")
+        kmeans.fit(data)
+        sse.append(kmeans.inertia_)
+
+    knee = KneeLocator(K, sse, curve='convex', direction='decreasing')
+    
+    #printElbow(K, sse, knee)
+
+    return knee.knee
+
